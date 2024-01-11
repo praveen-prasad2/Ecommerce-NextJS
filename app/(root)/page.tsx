f40cb1023c03513fcbb9c589db2d841b7e8f729a
Praveen Prasad <praveenprasad2510@gmail.com>
2024-01-11 11:51:11 +0530

    Configured Clerk and created Signup and Signin Page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+<div className="p-20">
+   <Button className="m-24" >Click Me</Button>
+</div>
+  )
+}
